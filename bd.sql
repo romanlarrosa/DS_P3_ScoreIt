62a0c5e628ad9330a64f0b69ec3d2b8b0417ea84
romanlarrosa <58295696+romanlarrosa@users.noreply.github.com>
2020-05-08 09:39:11 +0200

    Updated project

---
+++ bd.sql
@@ -0,0 +1,38 @@
+-- Crear la base de datos --
+
+create user 'si'@'localhost' identified by '1234';
+create user 'si'@'%' identified by '1234';
+CREATE DATABASE ScoreIt;
+
+
+--Crear la tabla de usuarios
+
+create table USUARIOS(
+    id_usuario int AUTO_INCREMENT primary key,
+    nombre varchar(200) not null,
+    pass varchar(200) not null,
+    tipo varchar(200)
+);
+
+
+CREATE TABLE LOCALES(
+    id_local int AUTO_INCREMENT primary key,
+    nombre_local varchar(200) not null,
+    ubicacion varchar(200) not null,
+    descripcion text not null,
+    owner_local int,
+    FOREIGN KEY (owner_local) references USUARIOS(id_usuario)
+);
+
+create table PUNTUACIONES(
+    id_usuario int not null,
+    id_local int not null,
+    puntos int not null,
+    FOREIGN KEY (id_usuario) references USUARIOS(id_usuario),
+    FOREIGN KEY (id_local) references LOCALES(id_local),
+    primary key (id_local, id_usuario)
+);
+
+
+
+
